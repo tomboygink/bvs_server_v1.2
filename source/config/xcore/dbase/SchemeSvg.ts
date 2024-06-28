0faa94e01ea77943a4d9f41b03bb47d982310b37
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2024-06-28 09:50:08 +0500

    синхронизация

--- source/config/xcore/dbase/SchemeSvg.ts
+++ source/config/xcore/dbase/SchemeSvg.ts
@@ -1,43 +1,46 @@
 import { DBase, getDB } from "./DBase";
 
 export class SchemeSvgEntity {
-    id: number = 0;
-    id_devs_groups: number = 0;
-    svg: string = "";
-    constructor() { }
+  id: number = 0;
+  id_devs_groups: number = 0;
+  svg: string = "";
+  constructor() {}
 }
 
 export class SchemeSvg {
-    db: DBase;
-    args: any;
-    sess_code: string;
+  db: DBase;
+  args: any;
+  sess_code: string;
 
-    constructor(_args: any, _sess_code: string) {
-        this.db = getDB();
-        this.args = _args;
-        this.sess_code = _sess_code;
+  constructor(_args: any, _sess_code: string) {
+    this.db = getDB();
+    this.args = _args;
+    this.sess_code = _sess_code;
+  }
+
+  //Получение схемы при нажатии на группу
+  async selectSchemeSVG(): Promise<SchemeSvgEntity[]> {
+    var db_response = await this.db.query(
+      "SELECT * FROM scheme_svg WHERE id_devs_groups = " +
+        this.args.id_devs_groups
+    );
+    var result: SchemeSvgEntity[] = new Array();
+
+    for (var svg in db_response.rows) {
+      result.push(db_response.rows[svg]);
     }
+    return result;
+  }
 
-
-    //Получение схемы при нажатии на группу 
-    async selectSchemeSVG(): Promise<SchemeSvgEntity[]> {
-
-        var db_response = await this.db.query("SELECT * FROM scheme_svg WHERE id_devs_groups = " + this.args.id_devs_groups);
-        var result: SchemeSvgEntity[] = new Array();
-
-        for (var svg in db_response.rows) {
-            result.push(db_response.rows[svg]);
-        }
-        return result;
-    }
-
-    //Добавление/обновление схемы группы
-    async newUpdateSchemeSVG() {
-        var db_response = await this.db.query("UPDATE scheme_svg SET svg = \'" + this.args.group_svg +
-            "\' WHERE id_devs_groups = " + this.args.id_devs_groups + "RETURNING id");
-        return db_response.rows;
-
-    }
+  //Добавление/обновление схемы группы
+  async newUpdateSchemeSVG() {
+    var db_response = await this.db.query(
+      "UPDATE scheme_svg SET svg = '" +
+        this.args.group_svg +
+        "' WHERE id_devs_groups = " +
+        this.args.id_devs_groups +
+        "RETURNING id"
+    );
+    return db_response.rows;
+  }
 }
-
-
